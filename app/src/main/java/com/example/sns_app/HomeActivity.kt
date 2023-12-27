3493a589df047bf9113282320acb33c3dea06e81
CodingVirus <twominyong@gmail.com>
2023-12-27 20:23:24 +0900

    메인화면 구성 및 하단 아이콘 스타일 추가

--- app/src/main/java/com/example/sns_app/HomeActivity.kt
+++ app/src/main/java/com/example/sns_app/HomeActivity.kt
@@ -1,11 +1,21 @@
 package com.example.sns_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val test = findViewById<ImageButton>(R.id.btn_home)
+
+        test.setOnClickListener {
+            val intent = Intent(this, MyPageActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

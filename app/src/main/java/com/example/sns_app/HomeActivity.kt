35e94d029c3e0f5c1254f1504209e11a9211db9c
CodingVirus <twominyong@gmail.com>
2023-12-27 12:06:12 +0900

    Activity 추가 및 다국어 설정 구성

--- app/src/main/java/com/example/sns_app/HomeActivity.kt
+++ app/src/main/java/com/example/sns_app/HomeActivity.kt
@@ -1,17 +1,11 @@
 package com.example.sns_app
 
-import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import java.util.Locale
 
-class MainActivity : AppCompatActivity() {
-
-
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        setContentView(R.layout.activity_home)
     }
 }

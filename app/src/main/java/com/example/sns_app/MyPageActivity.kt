3eb6c9ff5e2bf919737ec3db6e79615c9e87d17e
CodingVirus <twominyong@gmail.com>
2023-12-31 14:53:32 +0900

    마이 페이지 대략적인 화면 구성

--- app/src/main/java/com/example/sns_app/MyPageActivity.kt
+++ app/src/main/java/com/example/sns_app/MyPageActivity.kt
@@ -2,10 +2,40 @@ package com.example.sns_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.GridLayout
+import com.google.android.material.tabs.TabLayout
 
 class MyPageActivity : AppCompatActivity() {
+
+    private val tabLayout: TabLayout by lazy { findViewById(R.id.tab_layout) }
+    private val postGridLayout: GridLayout by lazy { findViewById(R.id.grid_my_post) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_page)
+
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when(tab?.position) {
+                    0 -> {
+                        postGridLayout.visibility = View.VISIBLE
+                    }
+
+                    1 -> {
+                        postGridLayout.visibility = View.INVISIBLE
+                    }
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+        })
+
     }
 }
